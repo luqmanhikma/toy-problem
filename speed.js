19efdf6848c462a58e1f331fa988863290855880
luqman <luqmanito602@gmail.com>
2023-10-25 10:25:04 +0300

    resubmitted

--- speed.js
+++ speed.js
@@ -1,4 +1,33 @@
 
+// let prompt = require('prompt-sync')();// Importing the prompt-sync module to take user input
+
+// const speed = parseFloat(prompt("Enter the speed of the car (in km/h): "));// Prompt the user to enter the speed of the car
+
+// // Function to calculate demerit points based on speed
+// function calculateDemeritPoints(speed) {
+//     const speedLimit = 70; // Define the speed limit
+//     let demeritPoints = 0; // Initialize demerit points variable
+
+//     if (speed <= speedLimit) {
+//         console.log("Ok"); // If speed is within limit, print "Ok"
+//     } else {
+//         // Calculate demerit points for exceeding the limit
+//         demeritPoints = Math.floor((speed - speedLimit) / 5);
+
+//         if (demeritPoints > 12) {
+//             console.log("License suspended"); // If points exceed 12, return "License suspended"
+//         } else {
+//             console.log("Points:", demeritPoints); // Otherwise, return the calculated points
+//         }
+//     }
+// }
+
+// // Call the function with the user-provided speed
+// calculateDemeritPoints(speed);
+
+
+
+
 let prompt = require('prompt-sync')();// Importing the prompt-sync module to take user input
 
 const speed = parseFloat(prompt("Enter the speed of the car (in km/h): "));// Prompt the user to enter the speed of the car
@@ -24,7 +53,3 @@ function calculateDemeritPoints(speed) {
 
 // Call the function with the user-provided speed
 calculateDemeritPoints(speed);
-
-
-
-
